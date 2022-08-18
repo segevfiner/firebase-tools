574ece95850e50a4bb5c880319329c016da70c1d
Tyler Stark <theirondeveloper@gmail.com>
2022-08-17 21:36:20 -0500

    Update placeholder code to reflect updated hashing api

--- src/deploy/functions/cache/applyHash.ts
+++ src/deploy/functions/cache/applyHash.ts
@@ -9,27 +9,54 @@ export async function applyBackendHashToBackends(
   wantBackends: Record<string, Backend>,
   context: args.Context
 ) {
+  // envHash
+  const envHash = getEnvHash(wantBackends);
   for (const [codebase, wantBackend] of Object.entries(wantBackends)) {
     const source = context?.sources?.[codebase]; // populated earlier in prepare flow
-    await applyBackendHashToBackend(wantBackend, source);
+    const sourceV1Hash = await getSourceHash(source?.functionsSourceV1);
+    const sourceV2Hash = await getSourceHash(source?.functionsSourceV2);
+    applyBackendHashToBackend(wantBackend, envHash, sourceV1Hash, sourceV2Hash);
   }
 }
 
 /**
  * Updates {@link Backend}, applying a unique hash to each {@link Endpoint}.
  */
-export async function applyBackendHashToBackend(wantBackend: Backend, source?: args.Source) {
-  // Blocked by #4852
-  // const v1FunctionHash = await getBackendHash(wantBackend, source.functionsSourceV1);
-  // const v2FunctionHash = await getBackendHash(wantBackend, source.functionsSourceV2);
-  const v1FunctionHash = "Hash-v1" + wantBackend + source;
-  const v2FunctionHash = "Hash-v2" + wantBackend + source;
-
+export function applyBackendHashToBackend(
+  wantBackend: Backend,
+  envHash: string,
+  sourceV1Hash: string,
+  sourceV2Hash: string
+) {
   allEndpoints(wantBackend).forEach((endpoint: Endpoint) => {
+    const secretsHash = getSecretHash(endpoint);
     const isV2 = endpoint.platform === "gcfv2";
+    const sourceHash = isV2 ? sourceV2Hash : sourceV1Hash;
     // Blocked by #4866
-    // endpoint.hash = isV2 ? v2FunctionHash : v1FunctionHash;
-
-    endpoint.hash = isV2 ? v2FunctionHash : v1FunctionHash;
+    endpoint.hash = getEndpointHash(sourceHash, envHash, secretsHash);
   });
+}
+
+function getEnvHash(wantBackends: Record<string, Backend>) {
+  const backends = Object.values(wantBackends);
+  if (backends.length) {
+    return getEnvironmentVariablesHash(backends[0]);
+  }
+  return "";
+}
+
+function getEnvironmentVariablesHash(backend: Backend) {
+  return "UNBLOCKING_HELPER_PLEASE_DELETE_ME_BEFORE_ITS_TOO_LATE";
+}
+
+async function getSourceHash(pathToGeneratedPackageFile = "") {
+  return "UNBLOCKING_HELPER_PLEASE_DELETE_ME_BEFORE_ITS_TOO_LATE";
+}
+
+function getSecretHash(endpoint: Endpoint) {
+  return "UNBLOCKING_HELPER_PLEASE_DELETE_ME_BEFORE_ITS_TOO_LATE";
+}
+
+function getEndpointHash(sourceHash: string, envHash: string, secretsHash: string) {
+  return "UNBLOCKING_HELPER_PLEASE_DELETE_ME_BEFORE_ITS_TOO_LATE";
 }
